812b6c791af1b494a9f40c2b3425d001a1687e49
ramseyjiang <ramseyjiang@gmail.com>
2022-04-16 14:30:39 +1200

    update interface define

--- practices/coffee_pot_interface.go
+++ practices/coffee_pot_interface.go
@@ -5,29 +5,41 @@ import (
 )
 
 type Appliance interface {
-	TurnOn()
+	TurnOn(name string)
+	TurnOff()
 }
 
 type Fan string
 
-func (f Fan) TurnOn() {
-	fmt.Println("Spinning")
+func (f Fan) TurnOn(name string) {
+	fmt.Println(name)
+}
+
+func (f Fan) TurnOff() {
+	fmt.Println("Fan has been turned off.")
 }
 
 type CoffeePot string
 
-func (c CoffeePot) TurnOn() {
-	fmt.Println("Powering up")
+func (c CoffeePot) TurnOn(name string) {
+	fmt.Println(name)
 }
+
+func (c CoffeePot) TurnOff() {
+	fmt.Println("CoffeePot has been turned off.")
+}
+
 func (c CoffeePot) Brew() {
 	fmt.Println("Heating Up")
 }
 
 func main() {
-	var device Appliance
-	device = Fan("Windco Breeze")
-	device.TurnOn()
-	device = CoffeePot("LuxBrew")
-	device.TurnOn()
-	// device.Brew() // It will has an error, because Brew() didn't define in the interface Appliance.
+	var fan Appliance = Fan("Windco Breeze")
+	fan.TurnOn("Today is too hot.")
+	fan.TurnOff()
+
+	var coffeePot Appliance = CoffeePot("LuxBrew")
+	coffeePot.TurnOn("Black Coffee.")
+	coffeePot.TurnOff()
+	// coffeePot.Brew() // It will has an error, because Brew() didn't define in the interface Appliance.
 }
