aea0f57e957c95f1839eeaca55c2a5266da02510
ramseyjiang <ramseyjiang@gmail.com>
2022-04-18 21:07:23 +1200

    update deadlock and review multichanselect

--- usages/channel/deadlock/instance1.go
+++ usages/channel/deadlock/instance1.go
@@ -8,10 +8,8 @@ What if there are no other goroutines available, imagine all of them are sleepin
 That’s where deadlock error occurs crashing the whole program.
 
 If you are trying to read data from a channel but channel does not have a value available with it,
-it blocks the current goroutine and unblocks other in a hope that some goroutine will push a value to the channel.
-Hence, this read operation will be blocking. Similarly, if you are to send data to a channel,
-it will block current goroutine and unblock others until some goroutine reads the data from it.
-Hence, this sends operation will be blocking.
+it will block current goroutine and unblock others until some goroutines push a value into it.
+Hence, this sends operation will be blocked.
 */
 
 func main() {
