d89f988721a67921fe2958035701443dc86243b8
ramseyjiang <ramseyjiang@gmail.com>
2022-05-07 15:51:05 +1200

    add an example to understand main routine easily.

---
+++ usages/channel/mainroutine/instance.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	firstCase()
+	correctCase()
+}
+
+func firstCase() {
+	var wg = &sync.WaitGroup{}
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		// fmt.Println("main goroutine is run at ", time.Now())
+		go func() {
+			fmt.Println(i)
+			// fmt.Println("go goroutine is run at ", time.Now())
+			wg.Done()
+		}()
+	}
+
+	// Using a WaitGroup to prevent the main() Goroutine exiting before the loop finishes.
+	wg.Wait()
+}
+
+func correctCase() {
+	var wg = &sync.WaitGroup{}
+	fmt.Println("-----correctCase start-----")
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(i int) {
+			fmt.Println(i)
+			wg.Done()
+		}(i)
+	}
+
+	wg.Wait()
+	fmt.Println("-----correctCase end-----")
+}
