f228f2e6411a0c475cca1c7a2cd5bdcd51187c7b
ramseyjiang <ramseyjiang@gmail.com>
2022-05-29 15:16:45 +1200

    delete useless json default

--- usages/datatypes/json.go
+++ usages/datatypes/json.go
@@ -12,14 +12,28 @@ type response struct {
 	LastName   string   `json:"-"`
 }
 
+type request struct {
+	Apple    int16  `json:"apple"`
+	Lettuce  int16  `json:"lettuce"`
+	LastName string `json:"LastName,omitempty"`
+}
+
 func encodeUsage() {
 	mapA := map[string]int{"apple": 5, "lettuce": 7}
-	mapB, err := json.Marshal(mapA) // Marshal return is []byte and error. It means a slice and error. Click Marshal to see.
+	mapB, err := json.Marshal(&mapA) // Marshal return is []byte and error. It means a slice and error.
 	if err != nil {
 		log.Println(err)
 	}
-	log.Println("Json encode usage is as below:")
-	log.Println(string(mapB))
+	log.Printf("Json encode1 usage is as below: %v", string(mapB))
+
+	mapC := request{}
+	mapC.Apple = 5
+	mapC.Lettuce = 7
+	mapD, err := json.Marshal(&mapC)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Printf("Json encode2 usage is as below: %v", string(mapD))
 }
 
 func decodeUsage(str string) {
@@ -32,8 +46,7 @@ func decodeUsage(str string) {
 		panic(err)
 	}
 
-	log.Println("Json decode usage is as below:")
-	log.Println(res) // res type is defined in the struct at the top.
+	log.Printf("Json decode usage is as below: %v", res)
 	log.Printf("Page is %v, fruits are %v\n", res.PageNumber, res.Fruits)
 }
 
@@ -53,41 +66,4 @@ func main() {
 	// first_name is empty "" in the json, so after Unmarshal, it cannot show in the output.
 	// That's the difference from int and string during using json.
 	decodeUsage(receiver2)
-
-	// jsonTypes()
 }
-
-// type ServerConf struct {
-// 	Host         string        `json:",default=0.0.0.0"`
-// 	Port         int           `json:",range=[80,65535)"`
-// 	LogMode      string        `json:",options=[file,console]"`
-// 	Verbose      bool          `json:",optional"`
-// 	MaxConn      int           `json:",default=10000"`
-// 	Timeout      time.Duration `json:",default=3s"`
-// 	CpuThreshold int64         `json:",default=900,range=[0:1000]"`
-// }
-//
-// func jsonTypes() {
-// 	serverConf := ServerConf{}
-// 	log.Printf("Default Host is %v\n", serverConf.Host)
-// 	log.Printf("Default Port is %v\n", serverConf.Port)
-// 	log.Printf("Default LogMode is %v\n", serverConf.LogMode)
-// 	log.Printf("Default Verbose is %v\n", serverConf.Verbose)
-// 	log.Printf("Default MaxConn is %v\n", serverConf.MaxConn)
-// 	log.Printf("Default Timeout is %v\n", serverConf.Timeout)
-// 	log.Printf("Default CpuThreshold is %v\n", serverConf.CpuThreshold)
-//
-// 	server := `{"Host": "127.0.0.1", "CpuThreshold": -10}`
-// 	decodeServerJson(server)
-// }
-//
-// func decodeServerJson(str string) {
-// 	server := ServerConf{}
-//
-// 	if err := json.Unmarshal([]byte(str), &server); err != nil {
-// 		panic(err)
-// 	}
-//
-// 	log.Println("Json decode usage is as below:")
-// 	log.Println(server)
-// }
