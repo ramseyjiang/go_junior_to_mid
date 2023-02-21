094574ddacb803ae20ad707482ecb3adec0ce508
ramseyjiang <ramseyjiang@gmail.com>
2023-02-21 15:11:16 +1300

    fix bug

--- usages/channel/waitgroup/example.go
+++ usages/channel/waitgroup/example.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
 )
 
 /**
@@ -14,27 +13,26 @@ WaitGroup is a struct with a counter value which tracks how many goroutines were
 This counter when reaches zero, means all goroutines have done their job.
 */
 
-func service(wg *sync.WaitGroup, instance int) {
-	time.Sleep(2 * time.Second)
-	fmt.Println("Service called on instance", instance)
-
-	// Once we are done with whatever the goroutine was supposed to do, we need to call Done method to decrement the counter.
-	// Done method decrements the counter. It does not accept any argument, hence it only decrements the counter by 1.
-	wg.Done()
-}
-
 func main() {
 	var wg sync.WaitGroup // create WaitGroup empty struct
+
+	goRoutines := 5
+	wg.Add(goRoutines)
 
 	// After for loop has done executing, it still did not pass control to other goroutines.
 	// This is done by calling Wait method on wg like wg.Wait().
 	// This will block the main goroutine until the counter reaches 0.
 	// Once the counter reaches 0 from goroutines, main goroutine will unblock and starts executing further code.
-	for i := 1; i <= 5; i++ {
+	for i := 0; i < goRoutines; i++ {
 		// Add method accept type of int, that means delta can also be negative.
 		// That will be used when you know amount of goroutines, and then you can try to use -1 to make it work.
-		wg.Add(1) // increment counter
-		go service(&wg, i)
+		go func(goRoutineID int) {
+			fmt.Printf("ID:%d: Hello goroutines!\n", goRoutineID)
+
+			// Once we are done with whatever the goroutine was supposed to do, we need to call Done method to decrement the counter.
+			// Done method decrements the counter. It does not accept any argument, hence it only decrements the counter by 1.
+			wg.Done()
+		}(i)
 	}
 
 	// blocks here
