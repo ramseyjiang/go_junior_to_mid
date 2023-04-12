9f69981982364d47bae9410fc7b5ca4bfef89b22
ramseyjiang <ramseyjiang@gmail.com>
2023-04-12 18:01:13 +1200

    update opmap

--- practices/optimization/opmap/opmap_test.go
+++ practices/optimization/opmap/opmap_test.go
@@ -5,17 +5,39 @@ import (
 )
 
 func BenchmarkDefineMapFirstWay(b *testing.B) {
-	DefineMapFirstWay()
+	words := make(map[int]interface{})
+	words[1] = "7"
+	words[2] = "simple"
+	words[3] = "ways"
+	words[4] = "to"
+	words[5] = "optimise"
+	words[6] = "Golang"
 }
 
 func BenchmarkDefineMapSecWay(b *testing.B) {
-	DefineMapSecWay()
+	type str struct {
+		word string
+	}
+	var words = map[int]str{}
+	words[1] = str{"7"}
+	words[2] = str{"simple"}
+	words[3] = str{"ways"}
+	words[4] = str{"to"}
+	words[5] = str{"optimise"}
+	words[6] = str{"Golang"}
 }
 
 func BenchmarkDefineMapThirdWay(b *testing.B) {
-	DefineMapThirdWay()
+	words := make(map[int]string)
+	words[1] = "7"
+	words[2] = "simple"
+	words[3] = "ways"
+	words[4] = "to"
+	words[5] = "optimise"
+	words[6] = "Golang"
 }
 
 func BenchmarkDefineMapFourthWay(b *testing.B) {
-	DefineMapFourthWay()
+	words := map[int]string{1: "7", 2: "simple", 3: "ways", 4: "to", 5: "optimise", 6: "Golang"}
+	_ = words
 }
